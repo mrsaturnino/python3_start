61743c5d654e4aebc698d8859dbeef1d7622093a
Andre Saturnino <andre_santos2012@hotmail.com>
2023-03-21 17:16:12 -0300

    functions in sets

--- python3_andre_iacono_udemy/05_data_structures/04_sets.py
+++ python3_andre_iacono_udemy/05_data_structures/04_sets.py
@@ -1,1 +1,2 @@
 # Sets são similares às listas. Porém, não utilizam index e evitam itens duplicados.
+
---
+++ python3_andre_iacono_udemy/05_data_structures/05_functions_in_sets.py
@@ -0,0 +1,8 @@
+set1 = {1, 2, 3, 4, 5, 6}
+
+set1.update([5, 9, 60, 10]) #ao utilizar o método "update", ele adiciona ao set os itens e não adiciona os duplicados.
+
+#set1.remove()
+#set1.discard()
+
+print(set1)
